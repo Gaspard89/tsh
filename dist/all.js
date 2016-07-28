f65e7bc003e3f84f1ed641f7923b57879a4fe91b
Kasper Ślusarczyk <kasper.slusarczyk@gmail.com>
2016-07-28 18:57:39 +0200

    display data with Knockout JS

--- dist/all.js
+++ dist/all.js
@@ -1,26 +1,3 @@
-
-// var data = $.ajax({
-//   dataType: "json",
-//   url: 'http://test-api.kuria.tshdev.io/',
-//   data: data,
-// });
-//
-// console.log(data);
-// var myArr = JSON.parse(data.responseText);
-// console.log(myArr);
-
-
-// var url = 'http://test-api.kuria.tshdev.io/';
-// var j = [];
-// $.ajax({
-//   type: 'GET',
-//   url: url,
-//   dataType: 'json',
-//   success: function(data) { j = data;},
-//   async: false
-// });
-//
-// alert(j.Users[0].Name);
 $(function (){
 var url = "http://test-api.kuria.tshdev.io/";
 var data = $.parseJSON(
@@ -31,6 +8,7 @@ var data = $.parseJSON(
         }).responseText
     );
 
-    console.log(data.payments);
-ko.applyBindings(data);
-    });
+
+console.log(data.payments);
+ko.applyBindings(data, document.getElementById('listingTable'));
+});
